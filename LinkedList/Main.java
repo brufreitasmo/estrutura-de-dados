1617ccd4f97b9910bf8bd7208cfdb3350d8e2a47
brufreitasmo <brunafreitasmotta@gmail.com>
2024-07-29 20:42:48 -0300

    create main

---
+++ LinkedList/Main.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+        LinkedList list = new LinkedList();
+        list.add(10);
+        list.add(20);
+        list.add(1);
+        list.add(2);
+        list.add(100);
+
+        list.printList();
+
+    }
+
+
+}
