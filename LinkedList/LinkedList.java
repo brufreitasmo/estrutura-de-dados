421b1f276b9ee6190e6be3541cd61ae18cf2a832
brufreitasmo <brunafreitasmotta@gmail.com>
2024-07-29 20:42:33 -0300

    create linked list

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class LinkedList {
+
+    private Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(newNode);
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.getData() + " ");
+            temp = temp.getNext();
+        }
+        System.out.println();
+    }
+    
+}
