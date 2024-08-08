f0fbde83a1d8d15bc93da82ad124dbdfdaa7e166
brufreitasmo <brunafreitasmotta@gmail.com>
2024-08-07 22:01:01 -0300

    Node

---
+++ BinaryTree/Node.java
@@ -0,0 +1,33 @@
+package BinaryTree;
+
+public class Node {
+	
+    private int value;
+    private Node left;
+    private Node right;
+
+    Node(int value) {
+        this.value = value;
+        left = right = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+}
